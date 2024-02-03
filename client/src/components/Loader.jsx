3c315a4170589e76993c6bc24d1091285d0fef84
Dorothy Baker <tbakerdorothy@gmail.com>
2024-02-03 17:21:32 +0300

    loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { PuffLoader } from "react-spinners";
 
 export default function Loader() {
   return (
-    <div className="w-full h-[70vh] flex flex-col items-center justify-center text-center bg-black/5 text-gray-900">
+    <div className="w-full h-[70vw] flex flex-col items-center justify-center text-center bg-black/5 text-gray-900">
       <PuffLoader size={50} />
       <span className="font-semibold text-lg">Loading...</span>
     </div>
